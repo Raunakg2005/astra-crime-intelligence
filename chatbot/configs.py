3b9d41f41994b428869fec06d9b18bba5be34c5c
muskish <surelymuski@gmail.com>
2026-07-18 22:31:01 +0530

    Configure Zoho Catalyst deployment settings and fix chatbot port handling

--- chatbot/configs.py
+++ chatbot/configs.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass
 
 @dataclass
@@ -7,10 +8,11 @@ class ChatBotConfig:
     max_requests_per_day: int = 1000
     tokens_per_minute: int = 6000
     max_tokens_per_day: int = 500000
-    api_base_url: str = "http://127.0.0.1:8000"
+    api_base_url: str = f"http://127.0.0.1:{os.environ.get('X_ZOHO_CATALYST_LISTEN_PORT', '8000')}"
     api_timeout_s: int = 10
     
 chatbot_config = ChatBotConfig()
+
     
     
     
